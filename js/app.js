f89614b14298b63f2ce9e0d71cc1aea5ee337837
pqasmi <pmqas002@gmail.com>
2021-08-04 12:05:06 -0400

    added refresh game and stop game functionality

--- js/app.js
+++ js/app.js
@@ -10,6 +10,11 @@ ageId.innerText = `Age: ${0}`
 let toma = document.querySelector("#pic")
 let gameContainer = document.querySelector("#game")
 
+//Disabled game buttons
+document.getElementById("play").disabled = true
+document.getElementById("sleepButton").disabled = true
+document.getElementById("feedPet").disabled = true
+
 //Game Class
 class Game {
     constructor(name){
@@ -24,42 +29,28 @@ class Game {
 const gotchi = new Game("Tomagotchi")    
 
 const ageUp = () => {
-    if (gotchi.age > 20) {
+    if (gotchi.age > 20 || gotchi.hunger >20) {
         clearInterval(gotchi.age = 20)
-    }
+        // clearInterval(gotchi.hunger = 20)
+    } else {
         gotchi.age++
         ageId.innerText = `Age: ${gotchi.age}`
-        setInterval (gameEnd, 1000)
+        gameEnd()
+        playToma()}
         
     }
 
-const gameEnd = () => {
-    if (gotchi.hunger > 10 || gotchi.sleepiness > 10 || gotchi.age >20) {
-        clearInterval(gotchi.hunger = 10)
-        clearInterval(gotchi.sleepiness = 10)
-        let overGame = document.querySelector("#gameOver")
-        overGame.innerText = "GAME OVER"
-        overGame.style.color = "red"
-        overGame.style.fontSize = "100px"
-        overGame.style.background = "black"
-        overGame.style.margin = "25px 50px 25px 50px "
-        gotchi.hunger = 0
-        gotchi.sleepiness = 0
-        gotchi.boredom = 0
-        document.getElementById("play").disabled = true
-        document.getElementById("sleepButton").disabled = true
-        document.getElementById("feedPet").disabled = true
-}
-}
 
 const playToma = () => {
+    if (gotchi.age <20) {
     gameContainer.style.background = "pink"
     toma.src= "https://static.wikia.nocookie.net/tamagotchi/images/2/2b/Kuchipatchi_anime.PNG/revision/latest?cb=20110918052545"
     let randomN = Math.floor(Math.random()*300)
-    toma.style.margin =  randomN + 'px'
+    toma.style.margin =  randomN + 'px'  
     addHunger()
-    reduceSleep()
+    sleepToma()
     havingFun()
+    }  
 }
 
 const sleepOffLights = () => {
@@ -90,7 +81,7 @@ const sleepToma = () => {
 }
 
 const reduceSleep = () => {
-    if (gotchi.sleepiness>1) {
+    if (gotchi.sleepiness > 1) {
         gotchi.sleepiness--
         sleepId.innerText = `Sleepiness: ${gotchi.sleepiness}`
     }}
@@ -108,22 +99,56 @@ const addHunger = () => {
         hungerId.innerText = `Hunger: ${gotchi.hunger}`   
     }
 
+const gameEnd = () => {
+    if (gotchi.hunger > 20 || gotchi.sleepiness > 20 || gotchi.age >20) {
+        clearInterval(gotchi.hunger = 20)
+        clearInterval(gotchi.sleepiness = 20)
+        let overGame = document.querySelector("#gameOver")
+        overGame.innerText = "GAME OVER"
+        overGame.style.color = "red"
+        overGame.style.fontSize = "100px"
+        overGame.style.background = "black"
+        overGame.style.margin = "25px 50px 25px 50px "
+        gotchi.hunger = 20
+        gotchi.sleepiness = 20
+        gotchi.boredom = 20
+        gotchi.age = 20
+        hungerId.innerText = `Hunger: ${gotchi.hunger}` 
+        sleepId.innerText = `Sleepiness: ${gotchi.sleepiness}`
+        boreId.innerText = `Boredom: ${gotchi.boredom}`
+        ageId.innerText = `Age: ${gotchi.age}`
+        document.getElementById("play").disabled = true
+        document.getElementById("sleepButton").disabled = true
+        document.getElementById("feedPet").disabled = true
+}
+}
+// Function clears the score when refresh button is hit
+const clearScores = () => {
+    hungerId.innerText = `Hunger: ${0}` 
+    sleepId.innerText = `Sleepiness: ${0}`
+    boreId.innerText = `Boredom: ${0}`
+    ageId.innerText = `Age: ${0}`
+}
+
 //Code to update userName when playing
 
 const updateName = () => {
     let name = document.querySelector("#userName")
-    insertName.innerText = `Player: ${name.value}`
-    setInterval (ageUp, 1000)
+    insertName.innerText = `Player Name: ${name.value}`
+    setInterval (ageUp, 1000)  
+    document.getElementById("play").disabled = false
+    document.getElementById("sleepButton").disabled = false
+    document.getElementById("feedPet").disabled = false
 
 }
 
 // Select h1 so when "Submit is clicked it replace the UserName with user input"
 let insertName = document.querySelector("#displayUserName")
-let submit = document.querySelector("#enterName")
+let submit = document.querySelector("#submit")
 submit.addEventListener('click',updateName)
 
 let play = document.querySelector("#play")
-play.addEventListener('click', playToma)
+play.addEventListener('click', playToma )
 
 let turnOff = document.querySelector("#sleepButton")
 turnOff.addEventListener('click', sleepOffLights)
@@ -131,6 +156,6 @@ turnOff.addEventListener('click', sleepOffLights)
 let feedYourPet = document.querySelector("#feedPet")
 feedYourPet.addEventListener('click', feeding)
 
-// let bored = document.querySelector("#bore")
-// feedYourPet.addEventListener('click', getBored)
+let clearScore = document.querySelector("#refresh")
+clearScore.addEventListener('click', clearScores)
 
